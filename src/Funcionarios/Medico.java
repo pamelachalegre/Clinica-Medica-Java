c51e79a33407212162775d41d33fdc08d52dc6bf
Ana Paula <164682309+anapaulacrippa@users.noreply.github.com>
2024-07-22 20:51:32 -0300

    update Medico.java

--- src/Funcionarios/Medico.java
+++ src/Funcionarios/Medico.java
@@ -1,14 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Funcionarios;
 import Dados.Paciente;
 import Dados.Relatorio;
-/**
- *
- * @author home
- */
+import java.util.ArrayList;
+
 public class Medico {
     private String nome;
     private String crm;
@@ -16,38 +10,72 @@ public class Medico {
     private Paciente paciente;
     private Relatorio relatorioMedico;
     
+    // método construtor
     public Medico() {}
-    
-    public void cadastrarPaciente(Paciente paciente, boolean colesterol, boolean fumar, boolean beber, boolean cardio) {
-        paciente.setAlergias("");
-        paciente.setColesterol(colesterol);
-        paciente.setBeber(beber);
-        paciente.setFumar(fumar);
-        paciente.setDoencaCardio(cardio);
-        paciente.setCirurgias("");
+
+    public Medico(String nome, String crm, Paciente paciente, Relatorio relatorioMedico) {
+        this.nome = nome;
+        this.crm = crm;
+        this.paciente = paciente;
+        this.relatorioMedico = relatorioMedico;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCrm() {
+        return crm;
+    }
+    public void setCrm(String crm) {
+        this.crm = crm;
+    }
+
+    public Relatorio getRelatorioMedico() {
+        return relatorioMedico;
+    }
+    public void setRelatorioMedico(Relatorio relatorioMedico) {
+        this.relatorioMedico = relatorioMedico;
     }
     
-    public void atualizaPacienteAlergia(Paciente paciente, String alergias) { // MUDAR ALERGIAS PRA LISTA!!
-        paciente.setAlergias("");
+    public void cadastrarPaciente(Paciente paciente, boolean fumar, boolean beber, boolean colesterol, boolean diabete, boolean cardio, ArrayList<String> cirurgias, ArrayList<String> alergias) {
+        paciente.setFumar(fumar);
+        paciente.setBeber(beber);
+        paciente.setColesterol(colesterol);
+        paciente.setDiabete(diabete);
+        paciente.setDoencaCardio(cardio);
+        paciente.setCirurgias(cirurgias);
+        paciente.setAlergias(alergias);
+    }
+    
+    public void atualizaPacienteFuma(Paciente paciente, boolean fumar) {
+        paciente.setFumar(fumar);
+    }
+
+    public void atualizaPacienteBebe(Paciente paciente, boolean beber) {
+        paciente.setBeber(beber);
     }
     
     public void atualizaPacienteColesterol(Paciente paciente, boolean colesterol) {
         paciente.setColesterol(colesterol);
     }
     
-    public void atualizaPacienteBebe(Paciente paciente, boolean beber) {
-        paciente.setBeber(beber);
-    }
-    
-    public void atualizaPacienteFuma(Paciente paciente, boolean fumar) {
-        paciente.setFumar(fumar);
+    public void atualizaPacienteDiabete(Paciente paciente, boolean diabete) {
+        paciente.setDiabete(diabete);
     }
     
     public void atualizaPacienteDoencaCardio(Paciente paciente, boolean cardio) {
         paciente.setDoencaCardio(cardio);
     }
     
-    public void atualizaPacienteCirurgias(Paciente paciente, String cirurgias) { // MUDAR CIRURGIAS PARA LISTA!!!
-        paciente.setCirurgias("");
-    }  
+    public void atualizaPacienteCirurgias(Paciente paciente, ArrayList<String> cirurgias) {
+        paciente.setCirurgias(cirurgias);
+    }
+        
+    public void atualizaPacienteAlergia(Paciente paciente, ArrayList<String> alergias) { 
+        paciente.setAlergias(alergias);
+    }   
 }
