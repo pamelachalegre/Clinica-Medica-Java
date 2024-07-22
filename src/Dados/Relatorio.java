b0cd430a24c707c98655e720d3f7099cfb4d85b5
Ana Paula <164682309+anapaulacrippa@users.noreply.github.com>
2024-07-22 20:49:01 -0300

    update Relatorio.java

--- src/Dados/Relatorio.java
+++ src/Dados/Relatorio.java
@@ -1,16 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Dados;
 
-/**
- *
- * @author home
- */
 public class Relatorio {
+    // é um POJO
     private String receita;
     private String atestado;
     private String acompanhamento;
     private int clientesMes;
+
+    public String getReceita() {
+        return receita;
+    }
+    public void setReceita(String receita) {
+        this.receita = receita;
+    }
+
+    public String getAtestado() {
+        return atestado;
+    }
+    public void setAtestado(String atestado) {
+        this.atestado = atestado;
+    }
+
+    public String getAcompanhamento() {
+        return acompanhamento;
+    }
+    public void setAcompanhamento(String acompanhamento) {
+        this.acompanhamento = acompanhamento;
+    }
+
+    public int getClientesMes() {
+        return clientesMes;
+    }
+    public void setClientesMes(int clientesMes) {
+        this.clientesMes = clientesMes;
+    }   
 }
