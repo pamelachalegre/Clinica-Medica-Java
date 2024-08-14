8a62d3d606f7db418485ed460072ef9d79980875
Ana Paula <164682309+anapaulacrippa@users.noreply.github.com>
2024-08-14 14:33:36 -0300

    criacao da heranca, comentarios e padronizacao

--- src/Funcionarios/Medico.java
+++ src/Funcionarios/Medico.java
@@ -10,48 +10,37 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Medico {
-    /**
-     * Objeto identificado pelos atributos nome e crm, mas possui métodos para manipular outros objetos, como gerar um
-     * novo objeto Relatório e modificar características de um objeto Paciente.
-     */
-    //DADOS PESSOAIS:
-    private String nome;
+public class Medico extends Funcionario {
+    /*
+    Objeto que herda as características de *Funcionario*.
+    É identificado pelo atributo *crm*, e possui métodos para manipular outros objetos, 
+    como *Relatorio* e *Paciente*. Também é capaz de criar os objetos *Atestado*, *DeclaracaoAcompanhamento*,
+    e *Receita*.
+    */
     private String crm;
-    private ArrayList<Consulta> Atendimentos;
-   
-    //MÉTODOS CONSTRUTORES:
-    public Medico() {}
-    public Medico(String nome, String crm) { //CARACTERISTICAS DO MÉDICO
-        this.nome = nome;
-        this.crm = crm;
-        this.Atendimentos =  new ArrayList<Consulta>();
-    }
+    private ArrayList<Consulta> atendimentos;
     
-    //SETS E GETS PARA AS CARACTERISTICAS:
-    public String getNome() {
-        return nome;
+    //Método construtor:    
+    public Medico(String nome, String cpf, float salario, ArrayList<Consulta> listaConsultas, ArrayList<Paciente> listaPacientes, String crm, ArrayList<Consulta> atendimentos) {
+        super(nome, cpf, salario, listaConsultas, listaPacientes);
+        this.crm = crm;
+        this.atendimentos =  new ArrayList<Consulta>();
     }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-   
+
+    //Sets e Gets para os atributos:
     public String getCrm() {
         return crm;
     }
-    public void setCrm(String crm) {
-        this.crm = crm;
-    }
 
     public ArrayList<Consulta> getPacientesAtendidos() {
-        return Atendimentos;
+        return atendimentos;
     }
     public void setPacientesAtendidos(ArrayList<Consulta> atendidos) {
-        this.Atendimentos = atendidos;
+        this.atendimentos = atendidos;
     }
     
     public void cadastrarDadosPaciente(Consulta consulta, boolean fumar, boolean beber, boolean colesterol, boolean diabete, boolean doencaCardio, ArrayList<String> cirurgias, ArrayList<String> alergias) {
-        //CADASTRA OS DADOS DE SAÚDE DE UM PACIENTE
+        // Cadastra os dados de saúde de um objeto *Paciente*
         consulta.getPaciente().setFumar(fumar);
         consulta.getPaciente().setBeber(beber);
         consulta.getPaciente().setColesterol(colesterol);
@@ -59,10 +48,10 @@ public class Medico {
         consulta.getPaciente().setDoencaCardio(doencaCardio);
         consulta.getPaciente().setCirurgias(cirurgias);
         consulta.getPaciente().setAlergias(alergias);
-        this.Atendimentos.add(consulta);
+        this.atendimentos.add(consulta);
     }
     
-    //ATUALIZA OS DADOS DA SAÚDE DE UM PACIENTE 'paciente':
+    // Atualiza os dados de saúde de um objeto *Paciente*:
     public void atualizaPacienteFuma(Paciente paciente, boolean fumar) {
         paciente.setFumar(fumar);
     }
@@ -78,27 +67,27 @@ public class Medico {
     public void atualizaPacienteDoencaCardio(Paciente paciente, boolean doencaCardio) {
         paciente.setDoencaCardio(doencaCardio);
     }
+    
     /*
-    Por conta do polimorfismo de sobreposição aplicado aos sets dos atributos 'cirurgias' e 'alergias' do Paciente, há
-    também um polimorfismo nos métodos para a atualização desses dados por parte do Médico.
+    Por conta do polimorfismo de sobreposição aplicado aos sets dos atributos 'cirurgias' e 'alergias' do Paciente, 
+    há também um polimorfismo nos métodos para a atualização desses dados por parte do Médico.
     */
     public void atualizaPacienteCirurgias(Paciente paciente, ArrayList<String> cirurgias) {
-        //define uma lista de cirurgias para o paciente
+        // Define uma lista de cirurgias para o paciente
         paciente.setCirurgias(cirurgias);
     }
     public void atualizaPacienteCirurgias(Paciente paciente, String cirurgia) {
-        //adiciona uma nova cirurgia
+        // Adiciona uma nova cirurgia
         paciente.getCirurgias().add(cirurgia);
     }
     public void atualizaPacienteAlergia(Paciente paciente, ArrayList<String> alergias) { 
-        //define as alergias do paciente
+        // Define as alergias do paciente
         paciente.setAlergias(alergias);
     }
     public void atualizaPacienteAlergia(Paciente paciente, String alergia) { 
-        //define as alergias do paciente
+        // Define as alergias do paciente
         paciente.getAlergias().add(alergia);
     }
-    
     public void atualizarPaciente(Paciente paciente, char campo, String novoDado) {
         switch(campo){
             case 'F':
@@ -139,66 +128,68 @@ public class Medico {
         }
     }
     
+    // Métodos do prontuário:
     public void cadastrarProntuario(Consulta consulta, String sintomas, String diagnostico, String tratamento) {
-        /*
-        Cadastra o prontuário do paciente.
-        */
+        // Cadastra o prontuário do paciente
         Prontuario prontuario = new Prontuario(sintomas, diagnostico, tratamento);
         consulta.getPaciente().setProntuario(prontuario);
     }
-    
-    public void atualizarProntuario(Paciente paciente, String atualizacao, char mudanca) {
-        /*
-        Altera algum dado do prontuário do paciente
-        */
-        switch(mudanca) {
-            case 'S' -> paciente.getProntuario().setSintomas(atualizacao);
-            case 'D' -> paciente.getProntuario().setDiagnostico(atualizacao);
-            case 'T' -> paciente.getProntuario().setTratamento(atualizacao);
-            default -> {
-            }
+    public void atualizarProntuario(Paciente paciente, char campo, String novoDado) {
+        // Altera algum dado do prontuário do paciente
+        switch(campo) {
+            case 'S' -> paciente.getProntuario().setSintomas(novoDado);
+            case 'D' -> paciente.getProntuario().setDiagnostico(novoDado);
+            case 'T' -> paciente.getProntuario().setTratamento(novoDado);
+            default -> System.out.println("Campo inválido!");
         }
     }
-    
     public void removerProntuario(Paciente paciente) {
+        // Remove o prontuário de um paciente
         paciente.getProntuario().setSintomas(null);
         paciente.getProntuario().setDiagnostico(null);
         paciente.getProntuario().setTratamento(null);
     }
     
-    // METODOS DOS RELATORIOS:
-    public void criarReceita(Medico medico, Paciente paciente, String remedio, float dosagem, String modoUso, int vezesDia) {
-        Receita receita = new Receita(medico, paciente, remedio, dosagem, modoUso, vezesDia);
-        receita.imprimeReceita();
-    }
-
-    public void criarEImprimirAtestado(Medico medico, Paciente paciente, Prontuario prontuario, int diasAfastamento, String dataInicio) {
+    // Métodos dos relatórios:
+    public void gerarImprimirAtestado(Medico medico, Paciente paciente, Prontuario prontuario, int diasAfastamento, String dataInicio) {
+        // Gera e imprime um objeto *Atestado*
         Atestado atestado = new Atestado(medico, paciente, prontuario, diasAfastamento, dataInicio);
         atestado.imprimeAtestado();
     }
-
     public void gerarDeclaracao(Medico medico, Paciente paciente, Prontuario prontuario, String dataAcompanhamento, String parentescoAcompanhante, String nomeAcompanhante) {
+        // Gera um objeto *DeclaracaoAcompanhamento*
         DeclaracaoAcompanhamento declaracao = new DeclaracaoAcompanhamento(medico, paciente, prontuario, dataAcompanhamento, parentescoAcompanhante, nomeAcompanhante);
         declaracao.imprimeDeclaracao();
     }
-    
+    public void gerarReceita(Medico medico, Paciente paciente, String remedio, float dosagem, String modoUso, int vezesDia) {
+        // Gera um objeto *Receita*
+        Receita receita = new Receita(medico, paciente, remedio, dosagem, modoUso, vezesDia);
+        receita.imprimeReceita();
+    }
     public int clientesAtendidos(List<Consulta> listaConsultas, int mes, int ano) {
-        List<String> pacientesUnicos = new ArrayList<>(); // Lista para armazenar CPFs unicos
-        int numPacientes = 0; // Contador do numero de pacientes iniciando em 0
+        // Lista para armazenar CPFs únicos
+        // Contador do numero de pacientes iniciando em 0
+        List<String> pacientesUnicos = new ArrayList<>();
+        int numPacientes = 0;
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
         for (Consulta consulta : listaConsultas) {
-            LocalDate dataConsulta = LocalDate.parse(consulta.getData(), formatter); // Converte a String da data para LocalDate
+            // Converte a String da data para LocalDate
+            LocalDate dataConsulta = LocalDate.parse(consulta.getData(), formatter);
 
-            if (dataConsulta.getMonthValue() == mes && dataConsulta.getYear() == ano) {  // Verifica se o mês e o ano da consulta são iguais aos fornecidos
+            if (dataConsulta.getMonthValue() == mes && dataConsulta.getYear() == ano) {
+                // Verifica se o mês e o ano da consulta são iguais aos fornecidos
                 String cpf = consulta.getPaciente().getCpf();
 
-                if (pacientesUnicos.contains(cpf) == false) {  // Verifica se o CPF ja esta na lista de pacientes unicos
-                    pacientesUnicos.add(cpf);  // Se nao estiver, adiciona o CPF a lista de pacientes unicos e incrementa o contador
+                if (pacientesUnicos.contains(cpf) == false) {
+                    // Verifica se o CPF ja esta na lista de pacientes unicos
+                    // Se nao estiver, adiciona o CPF a lista de pacientes unicos e incrementa o contador
+                    pacientesUnicos.add(cpf);
                     numPacientes++;
                 }
             }
         }
-        return numPacientes; // Retorna o numero de pacientes unicos atendidos
+        // Retorna o numero de pacientes únicos atendidos
+        return numPacientes;
     }
 }
